e6debcca8745b5f0f26d7086ba6441a110bf6c2f
winter-x64 <itswinterx64@gmail.com>
2026-07-06 16:30:25 +0530

    feat(ports): add log levels and format errors as [usmp] [error_type]: <msg>

--- port/usmp_port_esp32.c
+++ port/usmp_port_esp32.c
@@ -25,15 +25,28 @@ void usmp_port_delay_ms(uint32_t ms) { vTaskDelay(pdMS_TO_TICKS(ms)); }
 uint32_t usmp_port_millis(void) { return (uint32_t)(esp_timer_get_time() / 1000ULL); }
 
 void usmp_port_log(char level, const char* tag, const char* msg) {
+  if (level == 'E') {
+    char lower_tag[64];
+    size_t i;
+    for (i = 0; i < sizeof(lower_tag) - 1 && tag[i] != '\0'; i++) {
+      char c = tag[i];
+      if (c >= 'A' && c <= 'Z') {
+        lower_tag[i] = (char)(c + ('a' - 'A'));
+      } else {
+        lower_tag[i] = c;
+      }
+    }
+    lower_tag[i] = '\0';
+    printf("[usmp] [%s]: %s\n", lower_tag, msg);
+    return;
+  }
+
   switch (level) {
     case 'I':
       ESP_LOGI(tag, "%s", msg);
       break;
     case 'W':
       ESP_LOGW(tag, "%s", msg);
-      break;
-    case 'E':
-      ESP_LOGE(tag, "%s", msg);
       break;
     case 'D':
       ESP_LOGD(tag, "%s", msg);
